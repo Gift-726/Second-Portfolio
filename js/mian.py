386cade922228df298f2698a42662a5ea90ebb5f
Gift Ayano <ayanogift@gmail.com>
2024-06-28 15:07:19 +0000

    first commit

---
+++ js/mian.py
@@ -0,0 +1,79 @@
+from "{name of mod}" import {car} 
+import logo and vs from art
+import random
+print(logo)
+score = 0
+person = random.randint(0, len(data))
+person2 = random.randint(0, len(data))
+is_correct = False
+
+def sya():
+    print(f"")
+    w
+def start_game():
+    if person = person2:
+        return
+        
+    print(f"compare A: {data[person]["Name"]} a {data[person]["Career"]} from {data[person]["country"]}")    
+    print(vs)
+    print(f"compare A: {data[person2]["Name"]} a {data[person2]["Career"]} from {data[person2]["country"]}") 
+    followers = input("who has more follwers? Type A or B:")
+    if  followers == "A":
+        if {data[person]["Name"]} > {data[person2]["Name"]}:
+            print("Youre right! current score: {score}"
+            is_correct = True
+        else:
+            is_correct = False
+    elif followers == "B":
+        if {data[person]["Name"]} < {data[person2]["Name"]}:
+            print("Youre right! current score: {score}"
+            is_correct = True
+        else:
+            is_correct = False
+start_game
+while is_correct:
+    score += 1
+    print(f"Youre right! current score: {score}")
+    start_game(person = person2, person2 = random.randint(0, len(data)))
+print(f"sorry, thats wrong. final score : {score}")
+
+#  import random
+# computer_guess = random.randint(1, 101)
+# print("Welcome to the Number Guessing Game!!!")
+# print("I'm thinking of a number between 1 and 100.")
+# level = input("Choose a difficulty. Type 'easy' or 'hard'").lower()
+
+# correct = False
+# if level == 'easy':
+#   attempts = 4
+#   print(computer_guess)
+
+#   while not correct:
+#     attempts -= 1
+#     print(f"You have {attempts} attempts remaining to guess the number.")
+#     guess = int(input("Make a guess: "))
+#     if computer_guess == guess:
+#       correct = True
+#       print(f"You got it! The answer was {computer_guess}.")
+#     elif computer_guess > guess:
+#       print("Too low")
+#     elif guess > computer_guess:
+#       print("Too high")
+#     elif attempts == 1:
+#       print('ufufuf')
+#       correct = True
+#       print("you have run out of guesses") 
+# elif level == 'hard':
+#     attepts = 9
+#     print(computer_guess)
+
+#     while not
+
+
+#      __      __       .__                                  __                              ________                       
+# /  \    /  \ ____ |  |   ____  ____   _____   ____   _/  |_  ____     _____ ___.__.   /  _____/_____    _____   ____  
+# \   \/\/   // __ \|  | _/ ___\/  _ \ /     \_/ __ \  \   __\/  _ \   /     <   |  |  /   \  ___\__  \  /     \_/ __ \ 
+#  \        /\  ___/|  |_\  \__(  <_> )  Y Y  \  ___/   |  | (  <_> ) |  Y Y  \___  |  \    \_\  \/ __ \|  Y Y  \  ___/ 
+#   \__/\  /  \___  >____/\___  >____/|__|_|  /\___  >  |__|  \____/  |__|_|  / ____|   \______  (____  /__|_|  /\___  >
+#        \/       \/          \/            \/     \/                       \/\/               \/     \/      \/     \/ 
+# */
